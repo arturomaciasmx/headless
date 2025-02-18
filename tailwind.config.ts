d0839f50c0ea383ba2fbf2034c74f7730c24cd6e
Arturo Macias <arturomaciasmx@gmail.com>
2025-02-18 09:52:31 -0800

    setup tailwindcss config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { plugin } from "postcss";
 import type { Config } from "tailwindcss";
 
 export default {
@@ -5,18 +6,48 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: [
-          "Inter",
-          "ui-sans-serif",
-          "system-ui",
-          "sans-serif",
-          "Apple Color Emoji",
-          "Segoe UI Emoji",
-          "Segoe UI Symbol",
-          "Noto Color Emoji",
-        ],
+        sans: ["var(--font-geist-sans)"],
+      },
+      keyframes: {
+        fadeIn: {
+          from: { opacity: "0" },
+          to: {
+            opacity: "1",
+          },
+        },
+        blink: {
+          "0%": { opacity: "0.2" },
+          "20%": { opacity: "1" },
+          "100%": { opacity: "0.2" },
+        },
+      },
+      animation: {
+        adeIn: "fadeIn 0.3s easy-in-out",
+        blink: "blink 1.4s both infinite",
       },
     },
   },
-  plugins: [],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+    // @ts-expect-error types
+    plugin((matchUtilities, theme) => {
+      matchUtilities(
+        {
+          // @ts-expect-error types
+          "animation-delay": (value) => {
+            return {
+              "animation-delay": value,
+            };
+          },
+        },
+        {
+          values: theme("transitionDelay"),
+        }
+      );
+    }),
+  ],
 } satisfies Config;
